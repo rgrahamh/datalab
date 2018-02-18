09c2e7019b9329232ff314f3faedc7ec3d9a2edc
Ryan Houck <ryan_houck@taylor.edu>
2018-02-17 21:56:54 -0500

    Committed the code for datalab up to this point, put a 'done' message on functions that I've completed since our last meeting.

---
+++ decl.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+ {"allOddBits", (funct_t) allOddBits, (funct_t) test_allOddBits, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"anyEvenBit", (funct_t) anyEvenBit, (funct_t) test_anyEvenBit, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"divpwr2", (funct_t) divpwr2, (funct_t) test_divpwr2, 2,
+    "! ~ & ^ | + << >>", 15, 2,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+ {"fitsShort", (funct_t) fitsShort, (funct_t) test_fitsShort, 1,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"float_half", (funct_t) float_half, (funct_t) test_float_half, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_i2f", (funct_t) float_i2f, (funct_t) test_float_i2f, 1,
+    "$", 30, 4,
+     {{1, 1},{1,1},{1,1}}},
+ {"float_neg", (funct_t) float_neg, (funct_t) test_float_neg, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"ilog2", (funct_t) ilog2, (funct_t) test_ilog2, 1, "! ~ & ^ | + << >>", 90, 4,
+  {{1, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isNotEqual", (funct_t) isNotEqual, (funct_t) test_isNotEqual, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isZero", (funct_t) isZero, (funct_t) test_isZero, 1, "! ~ & ^ | + << >>", 2, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"logicalNeg", (funct_t) logicalNeg, (funct_t) test_logicalNeg, 1,
+    "~ & ^ | + << >>", 12, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"multFiveEighths", (funct_t) multFiveEighths, (funct_t) test_multFiveEighths, 1,
+    "! ~ & ^ | + << >>", 12, 3,
+  {{-(1<<28)-1, (1<<28)-1},{TMin,TMax},{TMin,TMax}}},
+ {"negate", (funct_t) negate, (funct_t) test_negate, 1,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"oddBits", (funct_t) oddBits, (funct_t) test_oddBits, 0,
+    "! ~ & ^ | + << >>", 8, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"rempwr2", (funct_t) rempwr2, (funct_t) test_rempwr2, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+ {"rotateRight", (funct_t) rotateRight, (funct_t) test_rotateRight,
+   2, "! ~ & ^ | + << >>", 25, 3,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"satMul3", (funct_t) satMul3, (funct_t) test_satMul3, 1,
+    "~ & ^ | + << >>", 25, 3,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"thirdBits", (funct_t) thirdBits, (funct_t) test_thirdBits, 0,
+    "! ~ & ^ | + << >>", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"tmin", (funct_t) tmin, (funct_t) test_tmin, 0, "! ~ & ^ | + << >>", 4, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"trueThreeFourths", (funct_t) trueThreeFourths, (funct_t) test_trueThreeFourths, 1,
+    "! ~ & ^ | + << >>", 20, 4,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
---
+++ tests.c
@@ -0,0 +1,175 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+int test_allOddBits(int x) {
+  int i;
+  for (i = 1; i < 32; i+=2)
+      if ((x & (1<<i)) == 0)
+   return 0;
+  return 1;
+}
+int test_anyEvenBit(int x) {
+  int i;
+  for (i = 0; i < 32; i+=2)
+      if (x & (1<<i))
+   return 1;
+  return 0;
+}
+int test_divpwr2(int x, int n)
+{
+    int p2n = 1<<n;
+    return x/p2n;
+}
+int test_fitsShort(int x)
+{
+  short int sx = (short int) x;
+  return x == sx;
+}
+unsigned test_float_half(unsigned uf) {
+  float f = u2f(uf);
+  float hf = 0.5*f;
+  if (isnan(f))
+    return uf;
+  else
+    return f2u(hf);
+}
+unsigned test_float_i2f(int x) {
+  float f = (float) x;
+  return f2u(f);
+}
+unsigned test_float_neg(unsigned uf) {
+    float f = u2f(uf);
+    float nf = -f;
+    if (isnan(f))
+ return uf;
+    else
+ return f2u(nf);
+}
+int test_ilog2(int x) {
+  int mask, result;
+  /* find the leftmost bit */
+  result = 31;
+  mask = 1 << result;
+  while (!(x & mask)) {
+    result--;
+    mask = 1 << result;
+  }
+  return result;
+}
+int test_isNotEqual(int x, int y)
+{
+  return x != y;
+}
+int test_isZero(int x) {
+  return x == 0;
+}
+int test_logicalNeg(int x)
+{
+  return !x;
+}
+int test_multFiveEighths(int x)
+{
+  return (x*5)/8;
+}
+int test_negate(int x) {
+  return -x;
+}
+int test_oddBits(void) {
+  int result = 0;
+  int i;
+  for (i = 1; i < 32; i+=2)
+    result |= 1<<i;
+  return result;
+}
+int test_rempwr2(int x, int n)
+{
+    int p2n = 1<<n;
+    return x%p2n;
+}
+int test_rotateRight(int x, int n) {
+  unsigned u = (unsigned) x;
+  int i;
+  for (i = 0; i < n; i++) {
+      unsigned lsb = (u & 1) << 31;
+      unsigned rest = u >> 1;
+      u = lsb | rest;
+  }
+  return (int) u;
+}
+int test_satMul3(int x)
+{
+  if ((x+x+x)/3 != x)
+    return x < 0 ? 0x80000000 : 0x7FFFFFFF;
+  else
+    return 3*x;
+}
+int test_thirdBits(void) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i+=3)
+    result |= 1<<i;
+  return result;
+}
+int test_tmin(void) {
+  return 0x80000000;
+}
+int test_trueThreeFourths(int x)
+{
+  return (int) (((long long int) x * 3)/4);
+}
